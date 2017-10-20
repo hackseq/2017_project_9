9719aa2fece40ad01abd0711fac88c1f2c4ce880
morien <morien@dhcp-128-189-84-42.ubcsecure.wireless.ubc.ca>
2017-10-20 16:35:15 -0700

    improved code skeleton

--- skeleton.R
+++ skeleton.R
@@ -8,39 +8,53 @@ tree_test <- rtree(n = 20) #need phylo class to work with edges as vectors
 
 #prep code
 #read metadata
-
+metadata <- read.delim(file="/projects/git/2017_project_9/testing_files/dummy_metadata.txt", sep="\t", header=T)
 #read tree
+tree <- read.tree(file.path("/projects/git/2017_project_9/testing_files/", "newick_format_tree.tre"))
+#parameters
+exclude <- c("Bivalvia", "Corallinophycidae") #two clades from rank5
+collapse <- c("rank5")
+
+metadata <- metadata[1:100,]
+tree <- drop.tip(tree,tree$tip.label[-match(metadata[,1], tree$tip.label)])
+
 
 #inputs
-metadata <- read.delim(file="/projects/git/2017_project_9/testing_files/dummy_metadata.txt", sep="\t", header=T)
+metadata <- metadata
 tree <- tree
 cladestoexclude <- exclude
 rankN <- collapse
 
 #make phylo4 object
-tree_4 <- as(tree, "phylo4") #need phylo4 class to get nodes from tips
+tree4 <- as(tree, "phylo4") #need phylo4 class to get nodes from tips
 
 #select tree tips
-for (clade in unique(metadata[[rankN]]) {
-  if (clade in cladestoexclude) {
+nodes <- c()
+tipstocollapse <- c()
+for (clade in unique(metadata[[rankN]])) {
+  if (clade %in% cladestoexclude) {
     print(paste0("Excluding clade: ", clade))
   }
   else {
-   tipstocollapse <- metadata[(which(metadata[[rankN]] == clade)),1]
+   tipstocollapse <- append(tipstocollapse, metadata[which(metadata[[rankN]] == clade),1])
+   
+   #TODO FIX THIS, THESE TIP LABELS ARE COMING OUT AS INDICES NOT NAMES
   }
-  returnNodes(tipstocollapse, tree4)
 }
+nodes <- returnNodes(tipstocollapse, tree4)
 
 
 #select nodes and branch positions based on tree tips
 #from tips to nodes
 returnNodes <- function(x,y) {
-  nodes <- c()
+  z <- c()
   for (tip in x) {
-    node <- as.numeric(getNode(y, tip, type = c("tip"), missing = c("warn"))[[1]])
-    nodes <- append(nodes, node)
+    node <- as.numeric(getNode(y, tip, type = c("tip"), missing = c("OK"))[[1]])
+    if (node != "NA") {
+      z <- append(z, node)
+    }
   }
-  return(nodes)
+  return(z)
 }
 
 #eliminate redundancies, we only want to collapse once for each complete clade we are collapsing
