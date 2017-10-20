66867f1fa956d50a6a80edb145bd8c764daf6d63
morien <morien@bunspace.local>
2017-10-20 13:53:02 -0700

    code skeleton

--- skeleton.R
+++ skeleton.R
@@ -8,32 +8,40 @@ tree_test_4 <- as(tree_test, "phylo4") #need phylo4 class to get nodes from tips
 #tree_test <- read.tree("/projects/kelp_seagrass_18s/tree/filtered_input_tree.tre")
 plot(tree_test)
 
-
-#building code
-
+#prep code
 #read metadata
 
 #read tree
 
 #select tree tips
+foreach (clade in unique(metadata$rankN)) {
+  if (clade %in% cladestoexclude) {
+    do nothing
+  }
+  else {
+   tipstocollapse <- metadata$tips[which(metadata$rankN == clade)]
+  }
+  return(tipstocollapse)  
+}
 
 
-
-
+#select nodes and branch positions based on tree tips
 #from tips to nodes
 foreach tip {
   x <- as.numeric(getNode(tree_test_4, "t20", type = c("tip"), missing = c("warn"))[[1]])
   return(x)
 }
 
+#eliminate redundancies, we only want to collapse once for each complete clade we are collapsing
 foreach node {
   get parent from tree_test$edge[,1] where tree_test$edge[,2] is the child
   flag node for collapse on second appearance, do recursively
-  #collapse splitTree(tree, list(node=NODE, bp=tree$edge.length[which(tree$edge[,2]==nn)]))
   return(listofnodestocollapse)
 }
 
-#select nodes and branch positions based on tree tips
-select ancestors of tips, for each ancestor, compare number selected vs number possible, if equal, collapse (set to be collapsed)
+#for each node, do the collapsing
+foreach node in listofnodestocollapse {
+  reduced_tree <- splitTree(tree, list(node=NODE, bp=tree$edge.length[which(tree$edge[,2]==nn)]))
+  return(reduced_tree)
+}
 
-splitTree(tree, list(node=NODE, bp=tree$edge.length[which(tree$edge[,2]==nn)]))
