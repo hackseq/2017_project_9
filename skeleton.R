6fa98a86a10d61b3cbcc36ccac8153537ad778ef
morien <morien@bunspace.local>
2017-10-20 15:04:07 -0700

    improved code skeleton

--- skeleton.R
+++ skeleton.R
@@ -4,44 +4,55 @@ library(phytools)
 library(phylobase)
 
 tree_test <- rtree(n = 20) #need phylo class to work with edges as vectors
-tree_test_4 <- as(tree_test, "phylo4") #need phylo4 class to get nodes from tips
 #tree_test <- read.tree("/projects/kelp_seagrass_18s/tree/filtered_input_tree.tre")
-plot(tree_test)
 
 #prep code
 #read metadata
 
 #read tree
 
+#inputs
+metadata <- read.delim(file="/projects/git/2017_project_9/testing_files/dummy_metadata.txt", sep="\t", header=T)
+tree <- tree
+cladestoexclude <- exclude
+rankN <- collapse
+
+#make phylo4 object
+tree_4 <- as(tree, "phylo4") #need phylo4 class to get nodes from tips
+
 #select tree tips
-foreach (clade in unique(metadata$rankN)) {
+for (clade in unique(metadata$rankN)) {
   if (clade %in% cladestoexclude) {
-    do nothing
+    print(paste0("Excluding clade: ", clade))
   }
   else {
    tipstocollapse <- metadata$tips[which(metadata$rankN == clade)]
   }
-  return(tipstocollapse)  
+  returnNodes(tipstocollapse, tree4)
 }
 
 
 #select nodes and branch positions based on tree tips
 #from tips to nodes
-foreach tip {
-  x <- as.numeric(getNode(tree_test_4, "t20", type = c("tip"), missing = c("warn"))[[1]])
-  return(x)
+returnNodes <- function(x,y) {
+  nodes <- c()
+  for (tip in x) {
+    node <- as.numeric(getNode(y, tip, type = c("tip"), missing = c("warn"))[[1]])
+    nodes <- append(nodes, node)
+  }
+  return(nodes)
 }
 
 #eliminate redundancies, we only want to collapse once for each complete clade we are collapsing
-foreach node {
-  get parent from tree_test$edge[,1] where tree_test$edge[,2] is the child
-  flag node for collapse on second appearance, do recursively
+for (node in nodes) {
+  #get parent from tree_test$edge[,1] where tree_test$edge[,2] is the child
+  #flag node for collapse on second appearance, do recursively
   return(listofnodestocollapse)
 }
 
 #for each node, do the collapsing
-foreach node in listofnodestocollapse {
-  reduced_tree <- splitTree(tree, list(node=NODE, bp=tree$edge.length[which(tree$edge[,2]==nn)]))
+for (node in listofnodestocollapse) {
+  reduced_tree <- splitTree(tree, list(node=node, bp=tree$edge.length[which(tree$edge[,2]==node)]))
   return(reduced_tree)
 }
 
